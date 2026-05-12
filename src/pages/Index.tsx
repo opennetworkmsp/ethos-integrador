6a36c098b772523e87ff56fef3849f70528d084f
Skip <skip@usecurling.com>
2026-05-12 14:36:15 +0000

    Auto-commit after full sync (build simulation)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -370,7 +370,7 @@ export default function Index() {
                   ) : (
                     <>
                       <Send className="mr-2 h-4 w-4" />
-                      Disparar Webhook
+                      Disparar
                     </>
                   )}
                 </Button>
